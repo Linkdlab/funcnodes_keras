812951eeba4371ba49cb32b980b82899c56e6622
Kourosh Rezaei <kouroshrezaei90@gmail.com>
2024-08-29 10:30:12 +0200

    ignore dataset nodes

--- tests/test_all_nodes.py
+++ tests/test_all_nodes.py
@@ -1,12 +1,12 @@
 import sys
 import os
+import unittest
+import funcnodes_keras as fnmodule
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from all_nodes_test_base import TestAllNodesBase
-import funcnodes as fn
-import funcnodes_keras as fnmodule
-import dataclasses
-from . import (
+from all_nodes_test_base import TestAllNodesBase  # noqa E402
+
+from . import (  # noqa E402
     test_applications,
     test_datasets,
     test_losses,
@@ -14,7 +14,7 @@ from . import (
     test_metrics,
     test_optimizers,
 )
-import unittest
+
 
 sub_test_classes = []
 
@@ -32,8 +32,7 @@ for mod in (
 
 
 class TestAllNodes(TestAllNodesBase):
-    ### in this test class all nodes should be triggered at least once to mark them as testing
+    # in this test class all nodes should be triggered at least once to mark them as testing
     sub_test_classes = sub_test_classes
 
-    def test_all_nodes(self):
-        pass
+    ignore_nodes = fnmodule.DATASETS_NODE_SHELFE["nodes"] + []
