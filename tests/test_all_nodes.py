1450b8647128935a133c8c8f345ad9b0fe53101a
Kourosh <kouroshrezaei90@gmail.com>
2025-01-09 13:41:44 +0100

    bump version to 0.1.14 and simplify test imports

--- tests/test_all_nodes.py
+++ tests/test_all_nodes.py
@@ -2,7 +2,7 @@ import unittest
 import funcnodes_keras as fnmodule
 from funcnodes_keras import fit
 from funcnodes import lib
-from all_nodes_test_base import TestAllNodesBase  # noqa E402
+from tests.all_nodes_test_base import TestAllNodesBase  
 import test_applications, test_losses, test_fit, test_metrics, test_optimizers, test_utilities  # noqa E402
 
 
