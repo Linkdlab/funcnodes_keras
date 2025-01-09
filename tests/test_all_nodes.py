36d38716ae0a8542355bf9a02f6a64e03d55fbd3
Kourosh <kouroshrezaei90@gmail.com>
2025-01-09 13:37:26 +0100

    bump version to 0.1.14 and simplify test imports

--- tests/test_all_nodes.py
+++ tests/test_all_nodes.py
@@ -1,21 +1,9 @@
-import sys
-import os
 import unittest
 import funcnodes_keras as fnmodule
 from funcnodes_keras import fit
 from funcnodes import lib
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from all_nodes_test_base import TestAllNodesBase  # noqa E402
-
-from . import (  # noqa E402
-    test_applications,
-    test_losses,
-    test_fit,
-    test_metrics,
-    test_optimizers,
-    test_utilities,
-)
+import test_applications, test_losses, test_fit, test_metrics, test_optimizers, test_utilities  # noqa E402
 
 
 sub_test_classes = []
